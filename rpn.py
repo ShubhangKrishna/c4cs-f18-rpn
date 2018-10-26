30981b794e5f7d34c5e0b985f3ee0f8f9e5d311d
shubhang <shubhang@umich.edu>
2018-10-26 11:17:03 -0400

    Passes first addition test

--- rpn.py
+++ rpn.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 def calculate(arg):
+    #stack for the calculator 
+    # take input and tokenize it 
+    #process tokens 
+    stack =[]
+    tokens = arg.split()
+    for token in tokens:
+        try: 
+            value= int (token)
+            stack.append(value)
+        except ValueError:
+            val1=stack.pop()
+            val2=stack.pop()
+            stack.append((val1+val2)) 
+            return stack[0]
+        
     pass
 def main():
     while True:
