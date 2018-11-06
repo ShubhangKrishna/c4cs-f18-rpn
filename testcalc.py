78f1164cbf54410c54ee6236cfe9f1b8c6ceb1be
Shubhang <shubhang99@ubuntu>
2018-11-06 00:12:38 -0500

    Calc GUI Working

--- testcalc.py
+++ testcalc.py
@@ -1,53 +1,24 @@
-from Tkinter import*
-import rpn.py 
+from Tkinter import *
+from rpnfgui import calculate 
 calculator = Tk()
 calculator.title('Calcualtor')
-calculator.geometry('221x319')
+calculator.geometry('250x319')
 
 screen = Frame(calculator, bd=2, width=300, height=325,bg='LightCyan')
 buttons = Frame(calculator, bd=2, width=300, height=325,bg='honeydew2')
 screen.grid(column=0, row=0, padx=25, pady=25)
 buttons.grid(column=0, row=1, padx=25)
 numbers=["7", "4", "1", "8", "5", "2", "9", "6", "3"]
-def lol():
-    t=results.get()
+def calc():
+
+    r=results.get()
     results.delete(0,END)
-    #print t
-    l=[]
-    l1=[]
-    l3=[]
-    for i in t:
-        if i!="+" and i!="-" and i!="/" and i!="*":
-            l.append(i)
-        else:
-            l1.append(i)
-            break
-     
-    for i in range((len(l)+1),len(t)):
-        y=t[i]
-        l3.append(y)
-    q=''
-    w=''
-    for i in l:
-        q=q+i
-    for i in l3:
-        w=w+i
-    q=float(q)
-    w=float(w)
-    #print q
-    #print w
-    priyanka=0.0
-    for i in l1:
-        if i=="+":
-            priyanka=q+w
-        elif i=="-":
-            priyanka=q-w
-        elif i=="*":
-            priyanka=q*w    
-        elif i=="/":
-            priyanka=q/w
-    #print priyanka
-    results.insert(0,priyanka)
+    s=""
+    for i in r: 
+        temp=str(i)
+        s=s+temp
+    ans=calculate(s)
+    results.insert(0,ans)
 def appear1():
     results.insert(END, "1")
     return
@@ -90,7 +61,15 @@ def appearDIV():
 def appearMUL():
     results.insert(END, "*")
     return
-
+def clear():
+    results.delete(0,END)
+    return
+def space():
+    results.insert(END," ")
+    return 
+def appearpow():
+    eruslts.insert(END,"^")
+    return 
 #Button(buttons, bg="White", text=numbers[index], width=5, height=2, appear).grid(padx=5, pady=5, row=index%3, column=index/3)
 Button(buttons, text = '1',bg='LightCyan',font='Helvetica 16 bold ',command=appear1).grid(row = 1, column = 0)
 Button(buttons, text = '2',bg='LightCyan',font='Helvetica 16 bold ',command=appear2).grid(row = 1, column = 1)
@@ -102,7 +81,12 @@ Button(buttons, text = '7',bg='LightCyan',font='Helvetica 16 bold ', command=app
 Button(buttons, text = '8',bg='LightCyan', font='Helvetica 16 bold ',command=appear8).grid(row = 3, column = 1)
 Button(buttons, text = '9',bg='LightCyan',font='Helvetica 16 bold ',command=appear9).grid(row = 3, column = 2)
 Button(buttons, text = '0',bg='LightCyan', font='Helvetica 16 bold ',command=appear0).grid(row = 4, column = 1)
-Button(buttons, text = '=',font='Helvetica 16 bold ',bg='LightCyan',command=lol).grid(row = 4, column = 0)
+Button(buttons, text = '=',font='Helvetica 16 bold ',bg='LightCyan',command=calc).grid(row = 4, column = 0)
+Button(buttons, text = 'C',font='Helvetica 16 bold ',bg='LightCyan',command=clear).grid(row = 6, column = 0)
+Button(buttons, text = 'S',font='Helvetica 16 bold ',bg='LightCyan',command=space).grid(row = 6, column = 1)
+Button(buttons, text = '^',font='Helvetica 16 bold ',bg='LightCyan',command=appearpow).grid(row = 4, column = 2)
+
+
 ##Button(buttons, text = 'exit', font='Helvetica 16 bold ',bg='LightCyan',).grid(row = 4, column = 3)
 Button(buttons, text = '+',font='Helvetica 16 bold ',bg='LightCyan',command=appearADD).grid(row = 5, column = 0)
 Button(buttons, text = '-',font='Helvetica 16 bold ' ,bg='LightCyan',command=appearSUB).grid(row = 5, column = 1)
